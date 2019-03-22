fd510a7dee1a9ee490f010df9ea6491487dec612
sbinder <sbinder@eggs.de>
2019-03-22 13:16:12 +0100

    fixed Eloquent model for training-participation pivot table (with attend attribute)

---
+++ server/app/TrainingParticipation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class TrainingParticipation extends Pivot
+{
+
+
+    protected $hidden = ['id', 'cancelreason'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function training()
+    {
+        return $this->belongsTo(Training::class);
+    }
+
+
+}
