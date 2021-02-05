0eebccf398ee5f0a77630046ecf6865adea77a19
Sebastian Binder <sbinder@eggs.de>
2021-02-05 12:10:58 +0100

    fixed approveUsers

--- app/src/pages/ApproveUsers.vue
+++ app/src/pages/ApproveUsers.vue
@@ -19,161 +19,160 @@
               </v-stepper-header>
               <v-stepper-items>
                 <v-stepper-content step="1">
-                  <v-card height="350">
-                    <v-container fluid fill-height row wrap>
-                      <v-layout
-                          justify-center
-                          align-center
-                      >
-                        <v-list
-                            two-line>
-                          <v-radio-group v-model="selectedNonApprovedUserId" :mandatory="false">
-                            <v-list-item
-                                v-for="(item) in nonApprovedUsers"
-                                :key="item.id"
-                            >
-                              <v-list-item-content>
-                                <v-list-item-title>{{ fullName(item) }}</v-list-item-title>
-                                <v-list-item-subtitle>
-                                  {{ moment(item.createdAt, 'YYYY-MM-DDTHH:mm').fromNow() + " registriert" }}
-                                </v-list-item-subtitle>
-                              </v-list-item-content>
-                              <v-list-item-action>
-                                <v-radio :value="item.id"></v-radio>
-                              </v-list-item-action>
-                            </v-list-item>
-                          </v-radio-group>
-                        </v-list>
-                      </v-layout>
+                  <v-card height="350" flat>
+                    <v-container>
+                      <v-row align="center"
+                             justify="center">
+                        <v-col md="6">
+                          <v-list
+                              two-line>
+                            <v-radio-group v-model="selectedNonApprovedUserId" :mandatory="false">
+                              <v-list-item
+                                  v-for="(item) in nonApprovedUsers"
+                                  :key="item.id"
+                              >
+                                <v-list-item-content>
+                                  <v-list-item-title>{{ fullName(item) }}</v-list-item-title>
+                                  <v-list-item-subtitle>
+                                    {{ moment(item.createdAt, 'YYYY-MM-DDTHH:mm').fromNow() + " registriert" }}
+                                  </v-list-item-subtitle>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                  <v-radio :value="item.id"></v-radio>
+                                </v-list-item-action>
+                              </v-list-item>
+                            </v-radio-group>
+                          </v-list>
+                        </v-col>
+                      </v-row>
                     </v-container>
                   </v-card>
-                  <v-btn
-                      color="primary"
-                      @click="approveWizardStep = 2"
-                      :disabled="!selectedNonApprovedUserId"
-                  >
-                    Weiter
-                    <v-icon right>arrow_forward</v-icon>
-                  </v-btn>
+                  <v-divider></v-divider>
+                  <v-container>
+                    <v-row justify="end">
+                      <v-btn
+                          color="primary"
+                          class="mr-2 mt-2"
+                          v-on:click="approveWizardStep = 2"
+                          :disabled="!selectedNonApprovedUserId"
+                      >
+                        Weiter
+                        <v-icon right>arrow_forward</v-icon>
+                      </v-btn>
+                    </v-row>
+                  </v-container>
                 </v-stepper-content>
                 <v-stepper-content step="2">
-                  <v-card height="350">
-                    <v-container fluid fill-height>
-                      <v-layout
-                          justify-center
-                          align-center
-                      >
-                        <GroupsSelect
-                            v-bind:groupIds="selectedGroupIds"
-                            v-on:groupsChanged="groupsChanged">
-                        </GroupsSelect>
-                      </v-layout>
+                  <v-card height="350" flat>
+                    <v-container>
+                      <v-row align="center"
+                             justify="center">
+                        <v-col md="6">
+                          <GroupsSelect
+                              v-bind:groupIds="selectedGroupIds"
+                              v-on:groupsChanged="groupsChanged">
+                          </GroupsSelect>
+                        </v-col>
+                      </v-row>
                     </v-container>
                   </v-card>
-                  <v-btn
-                      color="primary"
-                      @click="approveWizardStep = 1"
-                  >
-                    <v-icon left>arrow_back</v-icon>
-                    Zurück
-                  </v-btn>
-                  <v-btn
-                      color="primary"
-                      @click="approveWizardStep = 3"
-                      :disabled="!selectedGroupIds"
-                  >
-                    Weiter
-                    <v-icon right>arrow_forward</v-icon>
-                  </v-btn>
+                  <v-divider></v-divider>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="4">
+                        <v-btn
+                            color="primary"
+                            class="ml-2"
+                            @click="approveWizardStep = 1"
+                        >
+                          <v-icon left>arrow_back</v-icon>
+                          Zurück
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="4" offset="4" md="2" offset-md="6">
+                        <v-btn
+                            color="primary"
+                            class="mr-2"
+                            @click="approveWizardStep = 3"
+                            :disabled="!selectedGroupIds"
+                        >
+                          Weiter
+                          <v-icon right>arrow_forward</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-container>
                 </v-stepper-content>
                 <v-stepper-content step="3">
-                  <v-card height="350">
-                    <v-container fluid fill-height row wrap>
-                      <v-layout
-                          justify-center
-                          align-center
-                      >
-                        <v-container grid-list-md text-xs-center>
-                          <v-layout row wrap>
-                            <v-flex xs12>
-                              <v-flex xs1 md12>
-                                <v-alert
-                                    v-bind:value="$vuetify.breakpoint.lgAndUp && !nonRegisteredUserId"
-                                    type="info"
-                                    outlined
-                                    pa-1
-                                    ma-0
-                                    class="caption"
-                                    dismissible
-                                >
-                                  Ein vorübergehender Benutzer ohne E-Mail und Passwort kann hier dem Benutzer
-                                  zugeordnet werden. Damit werden alle Daten wie Trainingsteilnahmen dem Benutzer
-                                  übertragen und der
-                                  vorübergehende
-                                  Benutzer gelöscht. Dieser Schritt ist optional.
-                                </v-alert>
-                              </v-flex>
-                              <v-flex xs12>
-                                <v-tooltip bottom v-show="$vuetify.breakpoint.mdAndDown && !nonRegisteredUserId">
-                                  <v-btn
-                                      absolute
-                                      right
-                                      icon
-                                      color="primary"
-                                      slot="activator"
-                                      style="top: 10px;"
-                                  >
-                                    <v-icon>info</v-icon>
-                                  </v-btn>
-                                  Ein vorübergehender Benutzer ohne E-Mail und Passwort kann hier dem Benutzer
-                                  zugeordnet werden. Damit werden alle Daten wie Trainingsteilnahmen dem Benutzer
-                                  übertragen und der
-                                  vorübergehende
-                                  Benutzer gelöscht. Dieser Schritt ist optional.
-                                </v-tooltip>
-                                <v-alert
-                                    v-bind:value="nonRegisteredUserId"
-                                    type="warning"
-                                    class="text-small"
-                                    pa-0
-                                    ma-0
-                                    outlined
-                                >
-                                  Daten von <b>{{ nonRegisteredUserFullName }}</b> <i>(vorläufiger Benutzer)</i> werden
-                                  an <b>{{ nonApprovedUserFullName }}</b> <i>(Benutzer zum Freischalten)</i> übertragen.
-                                </v-alert>
-                              </v-flex>
-                              <v-flex xs12 md4 offset-md4>
-                                <v-autocomplete
-                                    v-bind:items="nonRegisteredUsers"
-                                    v-model="nonRegisteredUserId"
-                                    :item-text="fullName"
-                                    item-value="id"
-                                    clearable
-                                    label="Vorfläufige Benutzer">
+                  <v-card height="350" flat>
+                    <v-container>
+                      <v-row align="center"
+                             justify="center">
+                        <v-col cols="12">
+                          <v-alert
+                              :value="!nonRegisteredUserId"
+                                   type="info"
+                                   class="text-small"
+                                   pa-0
+                                   ma-0
+                                   outlined
+                          >
 
-                                </v-autocomplete>
-                              </v-flex>
-                            </v-flex>
-                          </v-layout>
-                        </v-container>
-                      </v-layout>
+                            Ein vorübergehender Benutzer ohne E-Mail und Passwort kann hier dem Benutzer
+                            zugeordnet werden. Damit werden alle Daten wie Trainingsteilnahmen dem Benutzer
+                            übertragen und der
+                            vorübergehende
+                            Benutzer gelöscht. Dieser Schritt ist optional.
+                          </v-alert>
+                          <v-alert
+                              :value="nonRegisteredUserId"
+                              type="warning"
+                              class="text-small"
+                              pa-0
+                              ma-0
+                              outlined
+                          >
+                            Daten von <b>{{ nonRegisteredUserFullName }}</b> <i>(vorläufiger Benutzer)</i> werden
+                            an <b>{{ nonApprovedUserFullName }}</b> <i>(Benutzer zum Freischalten)</i> übertragen.
+                          </v-alert>
+                        </v-col>
+                        <v-col cols="12" md="6">
+                          <v-autocomplete
+                              v-bind:items="nonRegisteredUsers"
+                              v-model="nonRegisteredUserId"
+                              :item-text="fullName"
+                              item-value="id"
+                              clearable
+                              label="Vorfläufige Benutzer">
+
+                          </v-autocomplete>
+                        </v-col>
+                      </v-row>
                     </v-container>
                   </v-card>
-                  <v-btn
-                      color="primary"
-                      @click="approveWizardStep = 2"
-                  >
-                    <v-icon left>arrow_back</v-icon>
-                    Zurück
-                  </v-btn>
-                  <v-btn
-                      color="primary"
-                      @click="approveUsers()"
-                  >
-                    <v-icon left>check</v-icon>
-                    Abschließen
-                  </v-btn>
+                  <v-divider></v-divider>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="4">
+                        <v-btn
+                            color="primary"
+                            @click="approveWizardStep = 2"
+                        >
+                          <v-icon left>arrow_back</v-icon>
+                          Zurück
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="4" offset="4">
+                        <v-btn
+                            color="primary"
+                            @click="approveUsers()"
+                        >
+                          <v-icon left>check</v-icon>
+                          Abschließen
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-container>
                   <v-dialog
                       v-model="confirmDialog"
                       width="500"
@@ -239,7 +238,7 @@
 
 <script>
 import {mapGetters} from 'vuex'
-import GroupsSelect from "../components/GroupsSelect";
+import GroupsSelect from "@/components/GroupsSelect";
 
 export default {
   name: "ApproveUsers",
@@ -248,7 +247,7 @@ export default {
     return {
       nonApprovedUsers: [],
       selectedNonApprovedUserId: null,
-      approveWizardStep: 0,
+      approveWizardStep: 1,
       selectedGroupIds: [],
       nonRegisteredUsers: [],
       nonRegisteredUserId: null,
